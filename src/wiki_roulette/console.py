61a3aa7e68cf5cb938950a9fa8eef59924ce6ca9
Thomas Teague <thomas.d.teague1@gmail.com>
2022-07-02 09:47:04 -0400

    Implement getting a random article from Wikipedia

--- src/wiki_roulette/console.py
+++ src/wiki_roulette/console.py
@@ -1,10 +1,28 @@
+import textwrap
+
 import click
+import requests
 
 from . import __version__
+
+# This URL is the REST API of the English Wikipedia that returns the summary of
+# a random article from Wikipedia.
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
 @click.version_option(version=__version__)
 def main() -> None:
-    """Print a message to the console."""
-    click.echo(message="Hello world!")
+    """Print a random Wikipedia article to the console."""
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(message=title, fg="green")
+
+    # The use of textwrap wraps the text so that every line is at most 70
+    # characters long.
+    click.echo(message=textwrap.fill(text=extract))
