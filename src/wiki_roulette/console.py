97473789f88c87ff67e1136955f63424e2ba41cc
Thomas Teague <thomas.d.teague1@gmail.com>
2022-07-02 09:53:49 -0400

    Handle the case when the API is unreachable

--- src/wiki_roulette/console.py
+++ src/wiki_roulette/console.py
@@ -14,15 +14,21 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main() -> None:
     """Print a random Wikipedia article to the console."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            data = response.json()
 
-    title = data["title"]
-    extract = data["extract"]
+        title = data["title"]
+        extract = data["extract"]
 
-    click.secho(message=title, fg="green")
+        click.secho(message=title, fg="green")
 
-    # The use of textwrap wraps the text so that every line is at most 70
-    # characters long.
-    click.echo(message=textwrap.fill(text=extract))
+        # The use of textwrap wraps the text so that every line is at most 70
+        # characters long.
+        click.echo(message=textwrap.fill(text=extract))
+
+    except requests.HTTPError:
+        click.secho(
+            message=f"The Wikipedia API at {API_URL} is unreachable.",
+            fg="red")
