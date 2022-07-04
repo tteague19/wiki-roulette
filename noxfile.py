04d9fb7433990922adbdc580e1e0f1ef0d93f6ed
Thomas Teague <thomas.d.teague1@gmail.com>
2022-07-04 11:32:02 -0400

    Add support for additional arguments to pytest

--- noxfile.py
+++ noxfile.py
@@ -9,5 +9,9 @@ def tests(session: nox.Session) -> None:
     :param session: A nox Session object
     :type session: nox.Session
     """
+    # If the first argument (a set of positional arguments) is non-empty, we
+    # use its contents. Otherwise, we default to the settings for coverage.py
+    # in the TOML file.
+    args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
