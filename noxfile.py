8d41553c9c739e4e9510e2474b26f4b3d2a92759
Thomas Teague <thomas.d.teague1@gmail.com>
2022-07-15 19:15:54 -0400

    Add function to pin requirements in nox sessions

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,31 @@ import nox
 # By default, we only run the linting, safety, and testing sessions.
 nox.options.sessions = "lint", "safety", "tests"
 
+# By default, we run Flake8 on the package source tree, the test suite,
+# noxfile.py itself. We can override this default by passing specific
+# source files separated from Nox's options by '--'.
+locations = ("src", "tests", "noxfile.py")
+
+
+def install_with_constraints(session: nox.Session, *args, **kwargs) -> None:
+    """
+    Use the Poetry lock file to pin packages used in nox sessions.
+
+    :param session: A nox Session object
+    :type session: nox.Session
+    """
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
 
 @nox.session(python="3.8")
 def black(session: nox.Session) -> None:
@@ -15,30 +40,8 @@ def black(session: nox.Session) -> None:
     :type session: nox.Session
     """
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
-
-
-@nox.session(python=["3.9", "3.10"])
-def tests(session: nox.Session) -> None:
-    """
-    Run tests with nox.
-
-    :param session: A nox Session object
-    :type session: nox.Session
-    """
-    # If the first argument (a set of positional arguments) is non-empty, we
-    # use its contents. Otherwise, we default to the settings for coverage.py
-    # in the TOML file.
-    args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
-    session.run("pytest", *args)
-
-
-# By default, we run Flake8 on the package source tree, the test suite,
-# noxfile.py itself. We can override this default by passing specific
-# source files separated from Nox's options by '--'.
-locations = ("src", "tests", "noxfile.py")
 
 
 @nox.session(python=["3.9", "3.10"])
@@ -53,7 +56,8 @@ def lint(session: nox.Session) -> None:
 
     # We install Flake8 into the virtual environment via pip with the
     # following command. We also add the plugin for Black and import order.
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -75,5 +79,24 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=["3.9", "3.10"])
+def tests(session: nox.Session) -> None:
+    """
+    Run tests with nox.
+
+    :param session: A nox Session object
+    :type session: nox.Session
+    """
+    # If the first argument (a set of positional arguments) is non-empty, we
+    # use its contents. Otherwise, we default to the settings for coverage.py
+    # in the TOML file.
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
+    session.run("pytest", *args)
